ebeea1c89308a9a369abca45285050d98b44fd56
My Name <5gunsisus@gmail.com>
2026-02-12 19:13:40 -0600

    updated web

--- index.web.js
+++ index.web.js
@@ -3,6 +3,37 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById('root'),
+
+try {
+  AppRegistry.runApplication(appName, {
+    rootTag: document.getElementById('root'),
+  });
+} catch (error) {
+  const root = document.getElementById('root');
+  root.innerHTML =
+    '<pre style="color:red;padding:20px;font-size:14px;">' +
+    error.message +
+    '\n\n' +
+    error.stack +
+    '</pre>';
+}
+
+window.addEventListener('error', function (event) {
+  const root = document.getElementById('root');
+  root.innerHTML =
+    '<pre style="color:red;padding:20px;font-size:14px;">Runtime Error:\n' +
+    event.message +
+    '\n\nFile: ' +
+    event.filename +
+    ':' +
+    event.lineno +
+    '</pre>';
+});
+
+window.addEventListener('unhandledrejection', function (event) {
+  const root = document.getElementById('root');
+  root.innerHTML =
+    '<pre style="color:red;padding:20px;font-size:14px;">Unhandled Promise:\n' +
+    event.reason +
+    '</pre>';
 });
